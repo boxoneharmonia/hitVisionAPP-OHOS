1f9a1d649c47be505f68912d010ae7e5c95bd134
boxone <974618020@qq.com>
2025-07-04 14:12:26 +0800

    完成内存和存储完整性校验，线程退出存在bug,暂时不影响

---
+++ entry/src/main/cpp/include/integrityCheck2.h
@@ -0,0 +1,13 @@
+//
+// Created on 2025/7/4.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#ifndef MYAPPLICATION_INTEGRITYCHECK2_H
+#define MYAPPLICATION_INTEGRITYCHECK2_H
+
+void startFileCheck();
+void stopFileCheck();
+
+#endif //MYAPPLICATION_INTEGRITYCHECK2_H
---
+++ entry/src/main/cpp/integrityCheck2.cpp
@@ -0,0 +1,53 @@
+//
+// Created on 2025/7/4.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#include "integrityCheckClass.h"
+#include "integrityCheck2.h"
+#include "log.h"
+#include "var.h"
+#include <thread>
+#include <atomic>
+#include <chrono>
+
+#define INTERVAL 5000
+#define MB 1024
+
+using namespace std;
+
+IntegrityChecker filechecker(1);
+static atomic<bool> threadRunning(false);
+
+static thread checkThread;
+
+static void checkLoop() {
+    while (threadRunning) {
+        filechecker.integrityCheck(fileCheckCnt, fileFaultCnt, fileResult);
+        std::this_thread::sleep_for(std::chrono::milliseconds(INTERVAL));
+    }
+}
+
+void startFileCheck() {
+//     LOGI("start File Check");
+    if (!threadRunning) {
+        filechecker.allocateIC(250 * MB, "/data/storage/el1/base/checkfile.bin");
+        threadRunning = true;
+        checkThread = thread(checkLoop);
+    } else {
+        LOGW("File Check has started");
+    }
+}
+
+void stopFileCheck() {
+    if (threadRunning) {
+        threadRunning = false;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        if (checkThread.joinable())
+            checkThread.join();
+        filechecker.releaseIC();
+    } else {
+        LOGW("File Check has stopped");
+    }
+}
